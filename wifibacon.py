349f6c492cee71fd194165a658d02a39d9ea24ed
Michael Hendrickx <michael@flurk.org>
2016-02-01 01:07:42 +0400

    some updates

--- wifibacon.py
+++ wifibacon.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 # gets wifi beacons
 # threaded and in wx
+# more info - @ndrix
+
 from scapy.all import *
-import os, sys, signal, wx, time
+import os, sys, signal, wx, time, subprocess
 from threading import Thread
 from wx.lib.pubsub import setuparg1
 from wx.lib.pubsub import pub as Publisher
 
 ap_list = {}
 client_list = {}
-iface = "wlan0"
+interfaces = []
 capturingPackets  = False
 
 # ################################################################################
 class SniffThread(Thread):
-  def __init__(self):
+  def __init__(self, iface):
     Thread.__init__(self)
     global capturingPackets
+    self.iface = str(iface)
     self.start()
 
   def PcapStopFilter(self, x):
     return capturingPackets == False
 
   def run(self):
-    sniff(iface=iface, prn = self.packetHandler) #, stop_filter = self.PcapStopFilter)
+    sniff(iface=self.iface, prn = self.packetHandler, stop_filter = self.PcapStopFilter)
 
   # our packet analysis
   def packetHandler(self, pkt):
@@ -39,7 +42,6 @@ class SniffThread(Thread):
         if pkt.haslayer(Dot11Elt):
           if pkt.ID == 0:
             if str(pkt.info) == "":
-              # print pkt.show()
               a = 1
             else:
               # a client send a probe request
@@ -65,18 +67,28 @@ class SniffThread(Thread):
 class MainWindow(wx.Frame):
   def __init__(self, parent, title):
     wx.Frame.__init__(self, None, wx.ID_ANY, "WIFI bac0n", size=(500,300))
+    self.Bind(wx.EVT_CLOSE, self.OnClose)
+
     panel = wx.Panel(self, wx.ID_ANY)
-    self.apListBox = wx.ListBox(choices=[], parent = panel, pos=wx.Point(8,48), size=(232,100))
-    self.clientListBox = wx.ListBox(choices=[], parent = panel, pos=wx.Point(250,48), size=(232,100))
+    self.apListBox = wx.ListBox(choices=[], parent = panel, pos=wx.Point(8,48), size=(232,200))
+    self.clientListBox = wx.ListBox(choices=[], parent = panel, pos=wx.Point(250,48), size=(232,200))
+
+    # list of interfaces
+    self.ifaceListBox = wx.ComboBox(choices=interfaces, value=interfaces[0], parent = panel, pos = wx.Point(8,8), size=(100, 30))
 
     # button to start sniffing
-    self.sniffButton = wx.Button(id=5, label=u'Sniff', parent=panel, pos=wx.Point(8,8))
+    self.sniffButton = wx.Button(id=5, label='Start capture', parent=panel, pos=wx.Point(120,8))
     self.sniffButton.Bind(wx.EVT_BUTTON, self.ClickSniffButton, id=5)
 
     Publisher.subscribe(self.AddAp, "addAp")
     Publisher.subscribe(self.AddClient, "addClient")
-                            
+
     self.Show(True)
+
+  def OnClose(self, event):
+    self.setWlanMon(self.ifaceListBox.GetValue(), False)
+    sys.exit(0);
+    
 
   def AddAp(self, msg):
     self.apListBox.Append(msg.data)
@@ -84,33 +96,56 @@ class MainWindow(wx.Frame):
   def AddClient(self, msg):
     self.clientListBox.Append(msg.data)
 
+  # set or bring adapter out of monitor mode
+  def setWlanMon(self, iface, mode = True):
+    try:
+      os.system('ifconfig %s down' % iface) 
+      os.system('iwconfig %s mode %s' % (iface, 'monitor' if mode else 'managed'))
+      os.system('ifconfig %s up' % iface)
+    except Exception:
+      msg = "Something went wrong setting %s in monitor mode" % iface
+      dlg = wx.MessageDialog(self, msg, "Warning", wx.OK | wx.ICON_WARNING)
+      dlg.ShowModal()
+      dlg.Destroy()
+      return False
+    return True
+    
   def ClickSniffButton(self, event):
-    SniffThread()
-    btn = event.GetEventObject()
-    btn.Disable()
+    global capturingPackets
+    global iface
+    if capturingPackets == False:
+      if self.setWlanMon(self.ifaceListBox.GetValue(), True):
+        print "[-] Started capture"
+        self.sniffButton.SetLabel("Stop capture")
+        self.ifaceListBox.Disable()
+        SniffThread(self.ifaceListBox.GetValue())
+        capturingPackets = True
+    else:
+      # we're capturing already, stop it
+      print "[-] Stopped capture"
+      self.ifaceListBox.Enable()
+      capturingPackets = False
+      self.sniffButton.SetLabel("Start capture")
+        
+
+
 
 # ################################################################################
-def setWlanMon(iface="wlan0", mode = True):
-  try:
-    os.system('ifconfig %s down' % iface) 
-    os.system('iwconfig %s mode %s' % (iface, 'monitor' if mode else 'managed'))
-    os.system('ifconfig %s up' % iface)
-  except Exception:
-    print "Something went wrong setting %s in monitor mode" % iface
-
-def ctrl_c(signal, frame):
-  print "Shutting down.."
-  setWlanMon(iface, False)
-  sys.exit(0);
 
 if __name__ == "__main__":  
   if os.geteuid() != 0:
     print "[!] Please run this script as root"
     exit();
-  if len(sys.argv) > 1:
-    iface = sys.argv[1]
-  signal.signal(signal.SIGINT, ctrl_c)
-  setWlanMon(iface)
+
+  # populate our list of WLAN cards
+  try:
+    out, err = subprocess.Popen('iwconfig', stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
+    for line in out.splitlines():
+      if "IEEE 802.11" in line:
+        interfaces.append(line.partition(' ')[0])
+  except Exception:
+    print "can't run iwconfig"
+
   capturingPackets = False
 
   # start our wx app
